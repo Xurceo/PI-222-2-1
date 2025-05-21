311c95e0a4696c07c1b11f8cda350d1b35c7b48b
Artem <minkovskijartem8@gmail.com>
2025-05-21 16:29:21 +0300

    UoW

---
+++ DAL/UoW/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Models;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<User> Users { get; }
+        IGenericRepository<Lot> Lots { get; }
+        IGenericRepository<Bid> Bids { get; }
+        IGenericRepository<Category> Categories { get; }
+
+        void Save();
+    }
+}
---
+++ DAL/UoW/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using DAL.Models;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AuctionDbContext _context;
+
+        private IGenericRepository<User>? _users;
+        private IGenericRepository<Lot>? _lots;
+        private IGenericRepository<Bid>? _bids;
+        private IGenericRepository<Category>? _categories;
+
+        public UnitOfWork(AuctionDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<User> Users =>
+            _users ??= new GenericRepository<User>(_context);
+
+        public IGenericRepository<Lot> Lots =>
+            _lots ??= new GenericRepository<Lot>(_context);
+
+        public IGenericRepository<Bid> Bids =>
+            _bids ??= new GenericRepository<Bid>(_context);
+
+        public IGenericRepository<Category> Categories =>
+            _categories ??= new GenericRepository<Category>(_context);
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool _disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
